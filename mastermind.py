8dd0dc80717b23b4b43b773ae0798bd0464de7e7
michaelgowie <michaelgowie1@gmail.com>
2025-04-26 12:19:11 +0100

    Adds option for player to request instructions before starting the game

--- mastermind.py
+++ mastermind.py
@@ -73,7 +73,7 @@ class Mastermind:
         rules_answer = input(
             print('Welcome to this game of Mastermind! Type Y to see the rules.'))
         if rules_answer == 'Y':
-            print('rules')
+            print('The aim of the game is to guess the correct colour combination of 5 pegs. Here, the 8 colours are represented by the 8 digits 1 through 8.\nGameplay \n- To take a turn, the player must input a combination of 5 digits from 1 to 8 as their guess. \n- The program will then compare this guess to the secret combination and return a set of up to five pegs, black or white.\n- If playing in easy mode, a black peg represents that the guessed digit in its position is correct, and in the right place, where a white peg represents that the guessed digit in its place is correct but in the wrong place.\n- If playing in hard mode, the colour of the pegs has the same meaning, but the position of teh pegs does not , and the black pegs will always be shown first, followed by the white pegs.\n- The player has 12 attempts to guess the combination.')
         easy_or_hard = ''
         while easy_or_hard != 'E' and easy_or_hard != 'H':
             easy_or_hard = input('Type E for easy mode or H for hard mode.')
@@ -96,8 +96,6 @@ class Mastermind:
                 f'Unlucky, you used your 12 attempts. The correct combination was {self.goal_combination}.')
 
 
-game = Mastermind()
-# print(game.ask_for_guess())
-
-# print(game.compare_guess_to_goal([1, 1, 1, 1, 5], [0, 1, 0, 0, 5]))
-game.play_game()
+if __name__ == '__main__':
+    game = Mastermind()
+    game.play_game()
